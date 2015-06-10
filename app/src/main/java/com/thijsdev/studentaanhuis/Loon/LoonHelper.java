3c63e52390a699a256cc682417ef8902eac6266c
Koozza <thijs@dalmaijer.net>
2015-06-10 03:03:24 +0200

    * Chilled HTTP calls the fuck down

--- app/src/main/java/com/thijsdev/studentaanhuis/Loon/LoonHelper.java
+++ app/src/main/java/com/thijsdev/studentaanhuis/Loon/LoonHelper.java
@@ -23,10 +23,10 @@ import static com.thijsdev.studentaanhuis.Reversed.reversed;
 public class LoonHelper {
     private int itemsAdded = 0;
     final TreeMap<Date, LoonMaand> loonMaandHashMap = new TreeMap<>();
+    final LoonHTTPHandler loonHTTPHandler = new LoonHTTPHandler();
 
     public void updateLoon(final Context context, final Callback callback) {
         final DatabaseHandler db = new DatabaseHandler(context);
-        final LoonHTTPHandler loonHTTPHandler = new LoonHTTPHandler();
 
         loonHTTPHandler.getMonths(context, new Callback() {
             @Override
@@ -49,95 +49,101 @@ public class LoonHelper {
                 }
 
                 final int totalItems = table.getElementsByTag("a").size();
-
-                //Loop trough it AGAIN now getting all details
-                for(Element e : table.getElementsByTag("a")) {
-                    //Get details for month
-                    String[] datumStringParts = GeneralFunctions.fixDate(e.children().get(0).text()).split(" ");
-                    loonHTTPHandler.getMonth(context, datumStringParts[1] + "-" + datumStringParts[0] + "-1", new Callback() {
-                        @Override
-                        public void onTaskCompleted(Object... results) {
-                            Document doc = Jsoup.parse((String) results[0]);
-                            Element tbody = doc.getElementsByTag("tbody").get(0);
-
-                            //Happens when there's no items in this month
-                            if (tbody != null) {
-                                Elements trs = tbody.select("tr:has(td)");
-                                for (Element tr : reversed(trs)) {
-                                    //Mogelijk voor de huidige maand
-                                    if (tr.children().get(3).text().equals("")) {
-                                        //Find first unpayed loonmaand
-                                        LoonMaand current = null;
-                                        for (LoonMaand loonMaand : loonMaandHashMap.values()) {
-                                            if (!loonMaand.isUitbetaald())
-                                                current = loonMaand;
-                                        }
-
-                                        //Calculate price
-                                        boolean isServiceVraag = tr.children().get(7).text().contains("-");
-                                        Double price = Double.parseDouble(tr.children().get(7).text().replace("-", "").substring(1).replace(",", "."));
-
-                                        //Check servicevraag
-                                        if (isServiceVraag) {
-                                            price = price * -1;
-                                            current.addServicevraag();
-                                        }
-
-                                        //check if uurloon
-                                        if (tr.children().get(1).text().contains("uurloon"))
-                                            current.addAfspraak();
-
-                                        current.addLoonMogelijk(price);
-                                    } else {
-                                        //Payment for sure
-                                        SimpleDateFormat format = new SimpleDateFormat("M yyyy");
-                                        try {
-                                            //Calculate date
-                                            String datumString;
-                                            if(!tr.children().get(4).text().equals(""))
-                                                datumString = tr.children().get(4).text();
-                                            else
-                                                datumString = tr.children().get(3).text();
-
-                                            String[] datumStringParts = datumString.split(" ");
-                                            Date date = format.parse(GeneralFunctions.fixDate(datumStringParts[1] + " " + datumStringParts[2]));
-                                            Calendar cal = Calendar.getInstance();
-                                            cal.setTime(date);
-                                            if(!tr.children().get(4).text().equals(""))
-                                                cal.add(Calendar.MONTH, -1);
-                                            date = cal.getTime();
-
-                                            //Calculate price
-                                            boolean isServiceVraag = tr.children().get(7).text().contains("-");
-                                            Double price = Double.parseDouble(tr.children().get(7).text().replace("-","").substring(1).replace(",", "."));
-
-                                            //Check servicevraag
-                                            if(isServiceVraag) {
-                                                price = price * -1;
-                                                loonMaandHashMap.get(date).addServicevraag();
-                                            }
-
-                                            //check if uurloon
-                                            if(tr.children().get(1).text().contains("uurloon"))
-                                                loonMaandHashMap.get(date).addAfspraak();
-
-                                            //set this month to uitbetaald
-                                            if(!tr.children().get(4).text().equals(""))
-                                                loonMaandHashMap.get(date).setIsUitbetaald(true);
-
-                                            loonMaandHashMap.get(date).addLoonZeker(price);
-                                        } catch (ParseException e1) {
-                                            e1.printStackTrace();
-                                        }
-                                    }
-                                }
-                                isFinalLoonUpdate(totalItems, callback);
-                            }
-                        }
-                    }, new Callback());
-                }
+                nextPage(table.getElementsByTag("a"), 0, context, totalItems, callback);
             }
         }, new Callback());
+    }
+
+    private void nextPage(final Elements elm, final int index, final Context context, final int totalItems, final Callback callback) {
+        //Get details for month
+        String[] datumStringParts = GeneralFunctions.fixDate(elm.get(index).children().get(0).text()).split(" ");
+        loonHTTPHandler.getMonth(context, datumStringParts[1] + "-" + datumStringParts[0] + "-1", new Callback() {
+            @Override
+            public void onTaskCompleted(Object... results) {
+                processPage((String) results[0], callback, totalItems);
+                if(index < elm.size() - 1)
+                    nextPage(elm, index + 1, context, totalItems, callback);
+            }
+        }, new Callback());
+    }
+
+    private void processPage(String source, Callback callback, int totalItems) {
+        Document doc = Jsoup.parse(source);
+        Element tbody = doc.getElementsByTag("tbody").get(0);
+
+        //Happens when there's no items in this month
+        if (tbody != null) {
+            Elements trs = tbody.select("tr:has(td)");
+            for (Element tr : reversed(trs)) {
+                //Mogelijk voor de huidige maand
+                if (tr.children().get(3).text().equals("")) {
+                    //Find first unpayed loonmaand
+                    LoonMaand current = null;
+                    for (LoonMaand loonMaand : loonMaandHashMap.values()) {
+                        if (!loonMaand.isUitbetaald())
+                            current = loonMaand;
+                    }
+
+                    //Calculate price
+                    boolean isServiceVraag = tr.children().get(tr.children().size() - 1).text().contains("-");
+                    Double price = Double.parseDouble(tr.children().get(tr.children().size() - 1).text().replace("-", "").substring(1).replace(",", "."));
+
+                    //Check servicevraag
+                    if (isServiceVraag) {
+                        price = price * -1;
+                        current.addServicevraag();
+                    }
+
+                    //check if uurloon
+                    if (tr.children().get(1).text().contains("uurloon"))
+                        current.addAfspraak();
+
+                    current.addLoonMogelijk(price);
+                } else {
+                    //Payment for sure
+                    SimpleDateFormat format = new SimpleDateFormat("M yyyy");
+                    try {
+                        //Calculate date
+                        String datumString;
+                        if(!tr.children().get(4).text().equals(""))
+                            datumString = tr.children().get(4).text();
+                        else
+                            datumString = tr.children().get(3).text();
+
+                        String[] datumStringParts = datumString.split(" ");
+                        Date date = format.parse(GeneralFunctions.fixDate(datumStringParts[1] + " " + datumStringParts[2]));
+                        Calendar cal = Calendar.getInstance();
+                        cal.setTime(date);
+                        if(!tr.children().get(4).text().equals(""))
+                            cal.add(Calendar.MONTH, -1);
+                        date = cal.getTime();
+
+                        //Calculate price
+                        boolean isServiceVraag = tr.children().get(tr.children().size() - 1).text().contains("-");
+                        Double price = Double.parseDouble(tr.children().get(tr.children().size() - 1).text().replace("-","").substring(1).replace(",", "."));
+
+                        //Check servicevraag
+                        if(isServiceVraag) {
+                            price = price * -1;
+                            loonMaandHashMap.get(date).addServicevraag();
+                        }
+
+                        //check if uurloon
+                        if(tr.children().get(1).text().contains("uurloon"))
+                            loonMaandHashMap.get(date).addAfspraak();
+
+                        //set this month to uitbetaald
+                        if(!tr.children().get(4).text().equals(""))
+                            loonMaandHashMap.get(date).setIsUitbetaald(true);
+
+                        loonMaandHashMap.get(date).addLoonZeker(price);
+                    } catch (ParseException e1) {
+                        e1.printStackTrace();
+                    }
+                }
+            }
+            isFinalLoonUpdate(totalItems, callback);
+        }
     }
 
     private void isFinalLoonUpdate(int totalItems, Callback callback) {
